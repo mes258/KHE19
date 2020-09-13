8fbff6b47068169ba7ce65bea5c7878e5fbc191d
mes258 <michael.smith.singapore@gmail.com>
2020-09-12 19:41:02 -0700

    added default config file

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ function allGameTypes(){
 var gameTypes = new allGameTypes();
 fs.appendFileSync('pids.txt', process.pid + "\n");
 
-var configFile = process.argv[2];
+var configFile = "config3030.txt";
 if(configFile == undefined){
   //console.error()
   console.error("Please specify a config file to use in the form: npm start config.txt");
